5fd004bb223e60754f5feed66089452025807293
Syb10 <56719752+Syb10@users.noreply.github.com>
2021-03-31 12:01:52 +0200

    added font-awesome

--- client/src/main.js
+++ client/src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.css'
 
 createApp(App).use(store).use(router).mount('#app')
