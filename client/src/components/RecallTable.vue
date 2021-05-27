e8b8d6aab84ef6f75d1800baa88ab0938f75b30f
Syb10 <56719752+Syb10@users.noreply.github.com>
2021-05-27 10:57:41 +0200

    renamed Recall component

---
+++ client/src/components/RecallTable.vue
@@ -0,0 +1,123 @@
+<template>
+  <div v-for="(n, index) in anzahl" :key="index" class="mb-4">
+    <p>{{n}}. 24-h-Recall</p>
+    <InputForm  v-for="(recallMahlzeit, index) in recallMahlzeiten" :key="index"
+        v-model="recallMahlzeit[n-1][0].datum"
+        :content="{ title: 'Datum', id: 'datumRecall'+n, type:'date' }"
+      />
+    <table class="mb-3 table table-striped table-bordered">
+        <thead>
+            <th>Mahlzeit</th>
+            <th>Uhrzeit</th>
+            <th>Wo wurde gegessen</th>
+            <th>Mit wem wurde gegessen</th>
+            <th>Lebensmittel und Getänke</th>
+            <th>Menge</th>
+            <th>Löschen</th>
+        </thead>
+        <tbody>
+            <tr v-for="(recallMahlzeit, index) in recallMahlzeiten['table'][n-1]" :key="index">
+                <td>
+                    <input class="form-control" type="text" v-model="recallMahlzeit.mahlzeit"/>
+                </td>
+                <td>
+                    <input class="form-control" type="text" v-model="recallMahlzeit.uhrzeit"/>
+                </td>
+                <td>
+                    <input class="form-control" type="text" v-model="recallMahlzeit.wo"/>
+                </td>
+                <td>
+                    <input class="form-control" type="text" v-model="recallMahlzeit.wer"/>
+                </td>
+                <td>
+                    <input class="form-control" type="text" v-model="recallMahlzeit.lebensmittelGetraenke"/>
+                </td>
+                <td>
+                    <input class="form-control" type="text" v-model="recallMahlzeit.menge"/>
+                </td>
+                <td>                   
+                    <i class="fa fa-trash btn btn-red ml-3" @click.prevent="deleteRow({tableIndex:n-1, rowIndex:index})"></i>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <button class="btn btn-gold mb-3" @click.prevent="addNewRow(n-1)"><i class="fa fa-plus-circle"></i> Mahlzeit hinzufügen</button>
+  </div>  
+</template>
+
+<script>
+import {mapGetters, mapMutations} from "vuex";
+import InputForm from "./InputForm.vue";
+
+  export default {
+    name: "RecallTable",
+     data() {
+      return {
+      }
+    },
+    components: {
+      InputForm
+    },
+    computed: {
+      ...mapGetters({
+        recallMahlzeiten: "recallMahlzeiten"
+      })
+    },
+    props: {
+      anzahl: Number
+    },
+    watch: {
+      anzahl:function(newValue, oldValue) {
+        var min = 0;
+        var max = 0;
+
+        if(newValue > oldValue) {
+          min = oldValue;
+          max = newValue;
+          //nur die differenz dazu
+          for(var i = min; i < max; i++){
+            this.addTable();
+            //this.addNewRow(i);
+          }
+        } else if (newValue < oldValue) {
+          min = newValue;
+          max = oldValue;
+          //von hinten loeschen sonst gibt es den Index nicht mehr
+          for(var j = max; j > min; j--){
+            this.deleteTable(j-1);
+          }
+        } else {
+          min = 0;
+          max = this.anzahl;
+          // am Anfang ist oldValue undefined
+          for(var k = min; k < max; k++){
+            this.addTable();
+            //this.addNewRow(k);
+          }
+        }
+      },
+    },
+    methods: {
+      ...mapMutations({
+        addTable: "addTable",
+        deleteTable: "deleteTable",
+        addNewRow: "addNewRow",
+        deleteRow: "deleteRow",
+        addDatum: "addDatum"
+      })
+    },
+  };
+</script>
+
+<style scoped>
+.btn-gold{
+    background-color: #9a884f !important;
+    border: 1px solid #9a884f !important;
+    color: white !important;
+}
+.btn-red{
+    background-color: #B75757 !important;
+    border: 1px solid #B75757 !important;
+    color: white !important;
+}
+</style>
