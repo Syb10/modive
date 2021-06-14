56947c6095fae62a4013f24f26588b2d90913027
Syb10 <56719752+Syb10@users.noreply.github.com>
2021-06-14 11:09:06 +0200

    RadioTable.vue for BilanzierungsTable.vue and Slider.vue RadioTableDynamic.vue for BilanzierungsTableDynamic.vue

---
+++ client/src/components/RadioTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <tr>
+    <td  v-for="(item, index) in items" :key="`item-${index}`" :class="item.class">
+      {{item.title}}
+      <div v-if="item.value" class="input-group">
+        <input
+          type="radio"
+          :id="item.id"
+          :name="item.name"           
+          :value="item.value"
+          v-model="value"
+        />
+      </div>
+    </td>
+  </tr>   
+</template>
+
+<script>
+  export default {
+    name:"RadioTable",
+    props:{
+      items:[],
+      modelValue:[],
+    },
+    emits:["update:modelValue"],
+    computed: {
+      value:{
+        get() {
+          return this.modelValue
+        },
+        set(value) {
+          this.$emit("update:modelValue", value)
+        }
+      },       
+    },
+  };
+</script>
+
+<style scoped>
+.paddingLeft{
+  padding: .75rem .75rem .75rem 2.25rem;
+}
+</style>
---
+++ client/src/components/RadioTableDynamic.vue
@@ -0,0 +1,163 @@
+<template>
+  <p v-if="subtitle" class="margin">
+    {{subtitle}}
+  </p> 
+  <!--for dynamic Bilanzierung-->
+  <tr v-show="isBilanzierung" v-for="(item, indexI) in DynamicBilanzierung" :key="`item-${indexI}`">
+    <td v-for="(i, index) in item" :key="`item-${index}`">
+      <input v-if="i.input" class="form-control" type="text" v-model="i.title"/>
+      <div v-if="i.value" class="input-group">
+        <input
+          type="radio"
+          :id="i.id"
+          :name="i.name"           
+          :value="i.value"
+          v-model="item[0].vmodel"
+        />
+      </div>
+      <i v-if="i.button" class="fa fa-trash btn btn-red ml-3" @click.prevent="deleteRowBilanzierung(indexI)"></i>
+    </td>
+  </tr>
+  <button v-if="isBilanzierung" class="btn btn-gold mb-3 margin" @click.prevent="addNewRowBilanzierung"><i class="fa fa-plus-circle"></i> Zeile hinzufügen</button> 
+  <!--for dynamic Andere-->
+  <tr v-show="isAndere" v-for="(item, indexI) in DynamicAndere" :key="`item-${indexI}`">
+    <td v-for="(i, index) in item" :key="`i-${index}`">
+      <input v-if="!i.value" class="form-control btnAndere" type="text" v-model="i.title"/>
+      <i v-if="!i.value" class="fa fa-trash btn btn-red ml-3" @click.prevent="deleteRowAndere(indexI)"></i>
+      <div v-if="i.value" class="input-group">
+        <input
+          type="radio"
+          :id="i.id"
+          :name="i.name"           
+          :value="i.value"
+          v-model="item[0].vmodel"
+        />
+      </div>
+    </td>
+  </tr>
+  <button v-if="isAndere" class="btn btn-gold mb-3 margin" @click.prevent="addNewRowAndere"><i class="fa fa-plus-circle"></i> Zeile hinzufügen</button> 
+</template>
+
+<script>
+  export default {
+    name:"RadioTable",
+    data() {
+      return {    
+        DynamicBilanzierung:[
+          [
+            {title: "", vmodel:"", input:true},
+            {value: "weniger",id:this.subtitle+"0Weniger",name:this.subtitle+"0"},
+            {value: "entsprechend der Ernährungsempfehlung",id:this.subtitle+"0Weniger",name:this.subtitle+"0"},
+            {value: "mehr",id:this.subtitle+"0Mehr",name:this.subtitle+"0"}, 
+            {button:true},
+          ]               
+        ],
+        DynamicAndere:[
+          [
+            {title:"", vmodel:""},
+            {value:"sehr wichtig", id:this.subtitle+"0sehrWichtig", name:this.subtitle+"0"},
+            {value:"wichtig", id:this.subtitle+"0wichtig", name:this.subtitle+"0"},
+            {value:"unwichtig", id:this.subtitle+"0unwichtig", name:this.subtitle+"0"},
+            {value:"sehr unwichtig", id:this.subtitle+"0sehrUnwichtig", name:this.subtitle+"0"},
+          ]
+        ],
+        counter:1
+      }
+    },
+    props:{
+      subtitle:String,
+      modelValue:[],
+      isBilanzierung:Boolean,
+      isAndere:Boolean,
+    },
+    emits:["update:modelValue"],
+    watch: {
+      // see https://v3.vuejs.org/guide/migration/watch.html#overview
+      DynamicBilanzierung:{
+        handler(value) {
+          var item=[]
+          //titel und v-model(wert) aus DynamicData an das v-model uebergeben aus assesment2
+          for(var i=0; i < value.length; i++){
+            if(i== value.length-1) {
+              //wenn es das letzte Paar ist kein Komma hinten
+              item.push(value[i][0].title,":", value[i][0].vmodel);
+            } else{
+              item.push(value[i][0].title,":", value[i][0].vmodel,", ");
+            }           
+          }
+          var newValue = item.join('')
+          this.$emit("update:modelValue", newValue)
+        },
+        deep: true
+      },
+      DynamicAndere:{
+        handler(value) {
+          var item=[]
+          //titel und v-model(wert) aus DynamicAndere an das v-model uebergeben aus assesment3
+          for(var i=0; i < value.length; i++){
+            if(i== value.length-1) {
+              //wenn es das letzte Paar ist kein Komma hinten
+              item.push(value[i][0].title,":", value[i][0].vmodel);
+            } else{
+              item.push(value[i][0].title,":", value[i][0].vmodel,", ");
+            }           
+          }
+          var newValue = item.join('')
+          this.$emit("update:modelValue", newValue)
+        },
+        deep: true
+      } 
+    },
+    methods: {
+      addNewRowBilanzierung() {
+        this.DynamicBilanzierung.push([
+          {title: "", vmodel:"",input:true},
+          {value: "weniger",id:this.subtitle+this.counter+"Weniger",name:this.subtitle+this.counter},
+          {value: "entsprechend der Ernährungsempfehlung",id:this.subtitle+this.counter+"Weniger",name:this.subtitle+this.counter},
+          {value: "mehr",id:this.subtitle+this.counter+"Mehr",name:this.subtitle+this.counter},
+          {button:true},
+        ]);
+        this.counter++;
+      },
+      addNewRowAndere() {
+        this.DynamicAndere.push([
+          {title: "", vmodel:""},
+          {value:"sehr wichtig", id:this.subtitle+this.counter+"sehrWichtig", name:this.subtitle+this.counter},
+          {value:"wichtig", id:this.subtitle+this.counter+"wichtig", name:this.subtitle+this.counter},
+          {value:"unwichtig", id:this.subtitle+this.counter+"unwichtig", name:this.subtitle+this.counter},
+          {value:"sehr unwichtig", id:this.subtitle+this.counter+"sehrUnwichtig", name:this.subtitle+this.counter},
+        ]);
+        this.counter++;
+      },
+      deleteRowBilanzierung(index) {
+        this.DynamicBilanzierung.splice(index, 1);
+      },
+      deleteRowAndere(index) {
+        this.DynamicAndere.splice(index, 1);
+      },
+    }
+  };
+</script>
+
+<style scoped>
+.btn-gold{
+    background-color: #9a884f !important;
+    border: 1px solid #9a884f !important;
+    color: white !important;
+}
+.btn-red{
+    background-color: #B75757 !important;
+    border: 1px solid #B75757 !important;
+    color: white !important;
+}
+.margin{
+  margin: 0.75rem;
+}
+.paddingLeft{
+  padding: .75rem .75rem .75rem 2.25rem;
+}
+.form-control.btnAndere{
+  display:inline !important;
+  width: 80%;
+}
+</style>
