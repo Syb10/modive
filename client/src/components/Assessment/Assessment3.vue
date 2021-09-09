7761d44ef4540e969db08a383efb19a08a39cb35
Syb10 <56719752+Syb10@users.noreply.github.com>
2021-09-09 16:02:26 +0200

    typo, added Wellbeing Index

--- client/src/components/Assessment/Assessment3.vue
+++ client/src/components/Assessment/Assessment3.vue
@@ -10,7 +10,7 @@
       <CheckboxForm
         :checkboxs="[
           {title:'gemeinschaftlich', id:'plantGemeinschaftlich'},
-          {title:'eigenstängig', id:'plantEigenstaengig'},
+          {title:'eigenständig', id:'plantEigenstaendig'},
           {title:'(Ehe-)Partner*in', id:'plantPartner'},
           {title:'Familienmitglied', id:'plantFamilienmitglied'}
         ]"
@@ -26,7 +26,7 @@
       <CheckboxForm
         :checkboxs="[
           {title:'gemeinschaftlich', id:'bereitetGemeinschaftlich'},
-          {title:'eigenstängig', id:'bereitetEigenstaengig',},
+          {title:'eigenständig', id:'bereitetEigenstaendig',},
           {title:'(Ehe-)Partner*in', id:'bereitetPartner'},
           {title:'Familienmitglied', id:'bereitetFamilienmitglied',},
           {title:'Außer-Haus-Verpflegung', id:'bereitetAusserHaus'},
@@ -41,10 +41,14 @@
         :content="{ title: 'andere', id: 'bereitetMahlzeitenAndere', type:'text', color:'assessmentColor', border:'assessmentBorder'}"
         :style="marginSmall"
       />
+      <TextareaForm
+        v-model="weitereAnmerkungenMahlzeiten"
+        :content="{ title: 'Weitere Anmerkungen zur Planung und Zubereitung von Mahlzeiten', id: 'weitereAnmerkungenMahlzeiten', color:'assessmentColor', border:'assessmentBorder'}"
+      />
       <CheckboxForm
         :checkboxs="[
           {title:'gemeinschaftlich', id:'einkaufGemeinschaftlich'},
-          {title:'eigenstängig', id:'einkaufEigenstaengig'},
+          {title:'eigenständig', id:'einkaufEigenstaendig'},
           {title:'(Ehe-)Partner*in', id:'einkaufPartner'},
           {title:'Familienmitglied', id:'einkaufFamilienmitglied'},
           {title:'Einkaufsservice', id:'einkaufEinkaufsservice'}
@@ -88,6 +92,8 @@
         <thead>
           <th>sehr einfach</th>
           <th>einfach</th>
+          <th>eher einfach</th>
+          <th>eher schwer</th>
           <th>schwer</th>
           <th>sehr schwer</th>
         </thead>
@@ -106,8 +112,10 @@
         :checkboxs="[
           {title:'Internet', id:'internet'},
           {title:'Zeitschriften', id:'zeitschriften'},
+          {title:'Fernsehen', id:'fernsehen'},
           {title:'Volkshochschule', id:'volkshochschule'},
-          {title:'Schule', id:'schule'}
+          {title:'Schule', id:'schule'},
+          {title:'nirgendwo', id:'nirgendwo'}
         ]"
         :title="'Wo informiert sich der/die Klient*in über Ernährung?'"
         :color ="'assessmentColor'"
@@ -119,14 +127,20 @@
         :style="marginSmall"
       />
       <TextareaForm
+        v-model="weitereAnmerkungenInformationsquellen"
+        :content="{ title: 'Weitere Anmerkungen zu den Informationsquellen über Ernährung', id: 'weitereAnmerkungenInformationsquellen', color:'assessmentColor', border:'assessmentBorder'}"
+      />
+      <TextareaForm
         v-model="orientierungErnaehrung"
-        :content="{ title: 'Woran orientiert sich der/die Klient*in bei seiner/ihrer Ernährung?', id: 'orientierungErnaehrung', color:'assessmentColor', border:'assessmentBorder'}"
+        :content="{ title: 'An welchen Empfehlungen orientiert sich der/die Klient*in bei seiner/ihrer Ernährung?', id: 'orientierungErnaehrung', color:'assessmentColor', border:'assessmentBorder'}"
       />
       <p>Wie wichtig ist es dem/der Klient*in, diesen Empfehlungen zu folgen?</p>
       <table class="table table-striped table-bordered mb-5">
         <thead>
           <th>sehr wichtig</th>
           <th>wichtig</th>
+          <th>eher wichtig</th>
+          <th>eher unwichtig</th>
           <th>unwichtig</th>
           <th>sehr unwichtig</th>
         </thead>
@@ -143,6 +157,8 @@
           <th>Für...</th>
           <th>sehr wichtig</th>
           <th>wichtig</th>
+          <th>eher wichtig</th>
+          <th>eher unwichtig</th>
           <th>unwichtig</th>
           <th>sehr unwichtig</th>
         </thead>
@@ -185,31 +201,31 @@
           <th>
             Absichtslosigkeit
             <div class="tipp ml-3"> <i class="fa fa-info-circle"></i>
-              <span class="tipptext">Er/sie denkt nicht daran, seine/ihre Ernährungsgewohnheiten zu verändern.</span>
+              <span class="tipptext">Er/sie denkt nicht daran, seine/ihre Ernährungsgewohnheiten in den nächsten Monaten zu verändern.</span>
             </div>
           </th>
           <th>
             Absichtsbildung
             <div class="tipp ml-3"> <i class="fa fa-info-circle"></i>
-              <span class="tipptext">Er/sie denkt darüber nach, seine/ihre Ernährungsgewohnheiten zu verändern.</span>
+              <span class="tipptext">Er/sie denkt darüber nach, seine/ihre Ernährungsgewohnheiten in den nächsten Monaten zu verändern.</span>
             </div>
           </th>
           <th>
             Vorbereitung
             <div class="tipp ml-3"> <i class="fa fa-info-circle"></i>
-              <span class="tipptext">Er/sie plant, in den nächsten 6 Monaten seine/ihre Ernährungsgewohnheiten zu verändern.</span>
+              <span class="tipptext">Er/sie plant, in den nächsten 30 Tagen seine/ihre Ernährungsgewohnheiten zu verändern.</span>
             </div>
           </th>
           <th>
             Handlung
             <div class="tipp ml-3"> <i class="fa fa-info-circle"></i>
-              <span class="tipptext">Er/sie plant definitiv im nächsten Monat seine/ihre Ernährungsgewohnheiten zu verändern.</span>
+              <span class="tipptext">Er/sie hat seine/ihre Ernährungsgewohnheiten seit weniger als sechs Monaten verändert.</span>
             </div>
           </th>
           <th>
             Aufrechterhaltung
             <div class="tipp ml-3"> <i class="fa fa-info-circle"></i>
-              <span class="tipptext aufrechterhaltung">Er/sie hat seine/ihre Ernährungsgewohnheiten bereits verändert.</span>
+              <span class="tipptext aufrechterhaltung">Er/sie behält seine/ihre veränderten Ernährungsgewohnheiten seit mehr als sechs Monaten bei.</span>
             </div>
           </th>         
         </thead>
@@ -245,6 +261,46 @@
         :myTipp="'Alltagsaktivität und Sport inklusive Dauer und Häufigkeit'"
       />
       <p class="assessmentAspekt" ref="lebensqualitaet">Lebensqualität</p>
+      <label>
+        WHO Wellbeing Index
+        <div class="tipp ml-3"> <i class="fa fa-info-circle"></i>
+          <span class="tipptext">Der Fragebogen zum WHO Wellbeing Index kann auch durch den/die Klient*in ausgefüllt und im Upload-Bereich hochgeladen werden.</span>
+        </div>
+      </label>
+      <table class="table table-striped table-bordered">
+        <thead>
+          <th>In den letzten zwei Wochen...</th>
+          <th>die ganze Zeit</th>
+          <th>meistens</th>
+          <th>etwas mehr als die Hälfte der Zeit</th>
+          <th>etwas weniger als die Hälfte der Zeit</th>
+          <th>ab und zu</th>
+          <th>zu keinem Zeitpunkt</th>
+        </thead>
+        <tbody>
+          <RadioTable
+            :items="laune"
+            v-model="whoLaune"
+          />
+          <RadioTable
+            :items="ruhig"
+            v-model="whoRuhig"
+          />
+          <RadioTable
+            :items="aktiv"
+            v-model="whoAktiv"
+          />
+          <RadioTable
+            :items="ausgeruht"
+            v-model="whoAusgeruht"
+          />
+          <RadioTable
+            :items="interessieren"
+            v-model="whoInteressieren"
+          />
+        </tbody>
+      </table>
+      <p><b>Lebensqualität: {{whoWert}} Punkte ({{whoProzent}}%) <i class="fa fa-arrow-right"></i> {{whoAuswertung}}</b></p> 
       <p>Upload-Bereich</p>
       <p class="assessmentAspekt" ref="upload">Upload-Bereich für weitere Dokumente in Verhalten und Umfeld (Behavioral-Environmental)</p>
     </div>
@@ -279,12 +335,16 @@ export default {
         mahlzeitZuzubereiten:[
           {value:"sehr einfach", id:"sehrEinfachMahlzeitZuzubereiten", name:"mahlzeitZuzubereiten"},
           {value:"einfach", id:"einfachMahlzeitZuzubereiten", name:"mahlzeitZuzubereiten"},
+          {value:"eher einfach", id:"eherEinfachMahlzeitZuzubereiten", name:"mahlzeitZuzubereiten"},
+          {value:"eher schwer", id:"eherSchwerMahlzeitZuzubereiten", name:"mahlzeitZuzubereiten"},
           {value:"schwer", id:"schwerMahlzeitZuzubereiten", name:"mahlzeitZuzubereiten"},
           {value:"sehr schwer", id:"sehrSchwerMahlzeitZuzubereiten", name:"mahlzeitZuzubereiten"},
         ],
         empfehlung:[
           {value:"sehr wichtig", id:"sehrWichtigEmpfehlung", name:"empfehlung"},
           {value:"wichtig", id:"wichtigEmpfehlung", name:"empfehlung"},
+          {value:"eher wichtig", id:"eherWichtigEmpfehlung", name:"empfehlung"},
+          {value:"eher unwichtig", id:"eherUnwichtigEmpfehlung", name:"empfehlung"},
           {value:"unwichtig", id:"unwichtigEmpfehlung", name:"empfehlung"},
           {value:"sehr unwichtig", id:"sehrUnwichtigEmpfehlung", name:"empfehlung"},
         ],
@@ -292,6 +352,8 @@ export default {
           {title:"...Frische"},
           {value:"sehr wichtig", id:"sehrWichtigFrische", name:"frische"},
           {value:"wichtig", id:"wichtigFrische", name:"frische"},
+          {value:"eher wichtig", id:"eherWichtigFrische", name:"frische"},
+          {value:"eher unwichtig", id:"eherUnwichtigFrische", name:"frische"},
           {value:"unwichtig", id:"unwichtigFrische", name:"frische"},
           {value:"sehr unwichtig", id:"sehrUnwichtigFrische", name:"frische"},
         ],
@@ -299,6 +361,8 @@ export default {
           {title:"...Geschmack"},
           {value:"sehr wichtig", id:"sehrWichtigGeschmack", name:"geschmack"},
           {value:"wichtig", id:"wichtigGeschmack", name:"geschmack"},
+          {value:"eher wichtig", id:"eherWichtigGeschmack", name:"geschmack"},
+          {value:"eher unwichtig", id:"eherUnwichtigGeschmack", name:"geschmack"},
           {value:"unwichtig", id:"unwichtigGeschmack", name:"geschmack"},
           {value:"sehr unwichtig", id:"sehrUnwichtigGeschmack", name:"geschmack"},
         ],
@@ -306,6 +370,8 @@ export default {
           {title:"...Gesundheit"},
           {value:"sehr wichtig", id:"sehrWichtigGesundheit", name:"gesundheit"},
           {value:"wichtig", id:"wichtigGesundheit", name:"gesundheit"},
+          {value:"eher wichtig", id:"eherWichtigGesundheit", name:"gesundheit"},
+          {value:"eher unwichtig", id:"eherUnwichtigGesundheit", name:"gesundheit"},
           {value:"unwichtig", id:"unwichtigGesundheit", name:"gesundheit"},
           {value:"sehr unwichtig", id:"sehrUnwichtigGesundheit", name:"gesundheit"},
         ],
@@ -313,6 +379,8 @@ export default {
           {title:"...Preis"},
           {value:"sehr wichtig", id:"sehrWichtigPreis", name:"preis"},
           {value:"wichtig", id:"wichtigPreis", name:"preis"},
+          {value:"eher wichtig", id:"eherWichtigPreis", name:"preis"},
+          {value:"eher unwichtig", id:"eherUnwichtigPreis", name:"preis"},
           {value:"unwichtig", id:"unwichtigPreis", name:"preis"},
           {value:"sehr unwichtig", id:"sehrUnwichtigPreis", name:"preis"},
         ],
@@ -320,6 +388,8 @@ export default {
           {title:"...Regionalität"},
           {value:"sehr wichtig", id:"sehrWichtigRegionalitaet", name:"regionalitaet"},
           {value:"wichtig", id:"wichtigRegionalitaet", name:"regionalitaet"},
+          {value:"eher wichtig", id:"eherWichtigRegionalitaet", name:"regionalitaet"},
+          {value:"eher unwichtig", id:"eherUnwichtigRegionalitaet", name:"regionalitaet"},
           {value:"unwichtig", id:"unwichtigRegionalitaet", name:"regionalitaet"},
           {value:"sehr unwichtig", id:"sehrUnwichtigRegionalitaet", name:"regionalitaet"},
         ],
@@ -327,6 +397,8 @@ export default {
           {title:"...Saisonalität"},
           {value:"sehr wichtig", id:"sehrWichtigSaisonalitaet", name:"saisonalitaet"},
           {value:"wichtig", id:"wichtigSaisonalitaet", name:"saisonalitaet"},
+          {value:"eher wichtig", id:"eherWichtigSaisonalitaet", name:"saisonalitaet"},
+          {value:"eher unwichtig", id:"eherUnwichtigSaisonalitaet", name:"saisonalitaet"},
           {value:"unwichtig", id:"unwichtigSaisonalitaet", name:"saisonalitaet"},
           {value:"sehr unwichtig", id:"sehrUnwichtigSaisonalitaet", name:"saisonalitaet"},
         ],
@@ -337,6 +409,51 @@ export default {
           {value:"Handlung", id:"handlung", name:"veraendern"},
           {value:"Aufrechterhaltung", id:"aufrechterhaltung", name:"veraendern"},
         ],
+        laune:[
+          {title:"...war der/die Klient*in froh und guter Laune."},
+          {value:5, id:"5Laune", name:"laune"},
+          {value:4, id:"4Laune", name:"laune"},
+          {value:3, id:"3Laune", name:"laune"},
+          {value:2, id:"2Laune", name:"laune"},
+          {value:1, id:"1Laune", name:"laune"},
+          {value:10, id:"0Laune", name:"laune"},
+        ],
+        ruhig:[
+          {title:"...hat sich der/die Klient*in ruhig und entspannt gefühlt."},
+          {value:5, id:"5Ruhig", name:"ruhig"},
+          {value:4, id:"4Ruhig", name:"ruhig"},
+          {value:3, id:"3Ruhig", name:"ruhig"},
+          {value:2, id:"2Ruhig", name:"ruhig"},
+          {value:1, id:"1Ruhig", name:"ruhig"},
+          {value:10, id:"0Ruhig", name:"ruhig"},
+        ],
+        aktiv:[
+          {title:"...hat sich der/die Klient*in energisch und aktiv gefühlt."},
+          {value:5, id:"5Aktiv", name:"aktiv"},
+          {value:4, id:"4Aktiv", name:"aktiv"},
+          {value:3, id:"3Aktiv", name:"aktiv"},
+          {value:2, id:"2Aktiv", name:"aktiv"},
+          {value:1, id:"1Aktiv", name:"aktiv"},
+          {value:10, id:"0Aktiv", name:"aktiv"},
+        ],
+        ausgeruht:[
+          {title:"...hat sich der/die Klient*in beim Aufwachen frisch und ausgeruht gefühlt."},
+          {value:5, id:"5Ausgeruht", name:"ausgeruht"},
+          {value:4, id:"4Ausgeruht", name:"ausgeruht"},
+          {value:3, id:"3Ausgeruht", name:"ausgeruht"},
+          {value:2, id:"2Ausgeruht", name:"ausgeruht"},
+          {value:1, id:"1Ausgeruht", name:"ausgeruht"},
+          {value:10, id:"0Ausgeruht", name:"ausgeruht"},
+        ],
+        interessieren:[
+          {title:"...war der Alltag des/der Klient*in voller Dinge, die er/sie interessieren."},
+          {value:5, id:"5Interessieren", name:"interessieren"},
+          {value:4, id:"4Interessieren", name:"interessieren"},
+          {value:3, id:"3Interessieren", name:"interessieren"},
+          {value:2, id:"2Interessieren", name:"interessieren"},
+          {value:1, id:"1Interessieren", name:"interessieren"},
+          {value:10, id:"0Interessieren", name:"interessieren"},
+        ],
       }
     },
     computed: {
@@ -345,6 +462,7 @@ export default {
         "plantMahlzeitenAndere",
         "bereitetMahlzeiten",
         "bereitetMahlzeitenAndere",
+        "weitereAnmerkungenMahlzeiten",
         "lebensmitteleinkauf",
         "lebensmitteleinkaufAndere",
         "selbstversorgen",
@@ -354,6 +472,7 @@ export default {
         "begruendungMahlzeitZuzubereiten",
         "informationErnaehrung",
         "informationErnaehrungAndere",
+        "weitereAnmerkungenInformationsquellen",
         "orientierungErnaehrung",
         "wichtigkeitDerEmpfehlung",
         "wichtigkeitDerFrische",
@@ -368,7 +487,33 @@ export default {
         "begruendungErnaehrungsgewohnheitenVeraendern",
         "physicalActivityLevel",
         "weitereAnmerkungenAktivitaet",
+        "whoLaune",
+        "whoRuhig",
+        "whoAktiv",
+        "whoAusgeruht",
+        "whoInteressieren",
       ]),
+      whoWert() {
+        var value = 0;
+         if(this.whoLaune) value += this.whoLaune == 10 ? 0 : this.whoLaune
+         if(this.whoRuhig) value += this.whoRuhig == 10 ? 0: this.whoRuhig
+         if(this.whoAktiv) value += this.whoAktiv == 10 ? 0: this.whoAktiv
+         if(this.whoAusgeruht) value += this.whoAusgeruht == 10 ? 0: this.whoAusgeruht
+         if(this.whoInteressieren) value += this.whoInteressieren == 10 ? 0: this.whoInteressieren
+        return value
+      },
+      whoProzent() {
+        return this.whoWert*4
+      },
+      whoAuswertung() {
+        var value=""
+        if(this.whoWert < 7) value = "Das Vorhandensein einer klinischen Depression ist sehr wahrscheinlich"
+        if(this.whoWert >= 7 && this.whoWert < 10) value = "deutlich eingeschränktes Wohlbefinden"
+        if(this.whoWert >= 10 && this.whoWert < 13) value = "Hinweis auf ein reduziertes Wohlbefinden"
+        if(this.whoWert >=13 && this.whoWert < 19) value = "ein insgesamt zufriedenstellendes Wohlbefinden"
+        if(this.whoWert >= 19) value = "ein sehr gutes Wohlbefinden"
+        return value
+      },
     },
     methods: {
       //https://stackoverflow.com/questions/42645964/vue-js-anchor-to-div-within-the-same-component
