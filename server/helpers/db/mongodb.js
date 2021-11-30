7b5a0edfe96bd5dca8edf87564ea43e100412e85
Syb10 <56719752+Syb10@users.noreply.github.com>
2021-11-30 15:11:39 +0100

    changed to cloud database

--- server/helpers/db/mongodb.js
+++ server/helpers/db/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
   
 const init = () => {
   mongoose
-    .connect(process.env.DB_URL2, {
+    .connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
